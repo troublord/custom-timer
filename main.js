a5f21ae0570621b12b3335dd8b50918b0920035e
troublord <yeelm1487@gmail.com>
2023-03-03 15:23:50 +0800

    some quick fix

--- main.js
+++ main.js
@@ -1,41 +1,71 @@
 window.onload = startClock;
 
 let alarmTime = new Date();
-let isCounting = false;
-
+let currentStatus;
 function startClock() {
+    initialStatus();
     const clockElement = document.querySelector(".clock");
     const clockObject = new DigitalClock(clockElement);
-
     clockObject.start();
+    
 }
 
 function startCount(){
-    isCounting = true;
+    currentStatus.isCounting = true;
+    currentStatus.isWorking = true;
     let alarmHour = document.querySelector('#alarm-time-hour').value;
     let alarmMinute = document.querySelector('#alarm-time-minute').value;
     alarmTime.setHours(alarmHour);
     alarmTime.setMinutes(alarmMinute);
     alarmTime.setSeconds(0);
+    showingStatus();
 }
 
+function initialStatus(){
+    let worktime = Number(document.querySelector('#work-time').value);
+    let resttime = Number(document.querySelector('#rest-time').value);
+    currentStatus = new Status(worktime,resttime,false,0,false); 
+}
+
+function showingStatus(){
+    document.querySelector('#status').style.display = 'block';
+    document.querySelector('#alarmTime').textContent = addZero(alarmTime.getHours()) + ":" + addZero(alarmTime.getMinutes());
+    updateStopwatch();
+}
+
+function resetStatus(){
+    let worktime = Number(document.querySelector('#work-time').value);
+    let resttime = Number(document.querySelector('#rest-time').value);
+    currentStatus.workTime = worktime
+    currentStatus.restTime = resttime;
+}
+class Status {
+    constructor(workTime, restTime,isWorking,totalTime,isCounting) {
+      this.workTime = workTime;
+      this.restTime = restTime;
+      this.isWorking = isWorking;
+      this.totalTime = totalTime; 
+      this.isCounting = isCounting;
+    }
+  }
 class DigitalClock {
     constructor(element) {
         this.element = element;
     }
     start() {
         this.update();
-        setInterval(this.update, 500);
+        setInterval(this.update, 1000);
     }
 
     update() {
         let currentTime = new Date();
-
         let hours = addZero(currentTime.getHours());
         let minutes = addZero(currentTime.getMinutes());
         document.querySelector('.clock-time').textContent = hours + ":" + minutes;
-        if(isCounting){
-            this.checkTime(currentTime);
+        if(currentStatus.isCounting){
+            currentStatus.totalTime++;
+            updateStopwatch();
+            this.checkTime(currentTime); // check if `this.` should be removed
         }
     }
 }
@@ -45,19 +75,20 @@ function checkTime(currentTime) {
     }
 }
 
-let isWorking = true;
-function alarm(currentTime) { 
+function alarm() { 
     let audio;
-    if (!isWorking) {
-        isWorking = true;
+    if (!currentStatus.isWorking) {
+        currentStatus.isWorking = true;
+        console.log(currentStatus.isWorking);
         audio = new Audio('Abreak.mp3');
         playRing(audio);
-        resetAlarm(10);
+        resetAlarm(currentStatus.restTime);
     } else {
-        isWorking = false;
+        currentStatus.isWorking = false;
+        console.log(currentStatus.isWorking);
         audio = new Audio('Beginning.mp3');
         playRing(audio);
-        resetAlarm(50);
+        resetAlarm(currentStatus.workTime);
     }
 }
 
@@ -78,4 +109,13 @@ function playRing(audio) {
 
 function resetAlarm(minute) {
     alarmTime.setMinutes(alarmTime.getMinutes() + minute);
+    document.querySelector('#alarmTime').textContent = addZero(alarmTime.getHours()) + ":" + addZero(alarmTime.getMinutes());
+    
+    document.querySelector('#isWorking').textContent = (!currentStatus.isWorking) ? "工作中" : "休息" ;
+}
+
+function updateStopwatch(){
+    const hours = addZero(Math.floor(currentStatus.totalTime / 3600));
+    const minutes = addZero(Math.floor((currentStatus.totalTime - hours * 3600) / 60));
+    document.querySelector('#totalTime').textContent = hours + ":" + minutes;
 }
