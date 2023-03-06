b192251001bc58bbc78f08875c7e19b5587e2689
troublord <yeelm1487@gmail.com>
2023-03-06 11:51:45 +0800

    bug fix n add stop button

--- main.js
+++ main.js
@@ -2,52 +2,24 @@ window.onload = startClock;
 
 let alarmTime = new Date();
 let currentStatus;
+
 function startClock() {
-    initialStatus();
+    currentStatus = new Status(false,0,false); 
+    // start clock running--------------
     const clockElement = document.querySelector(".clock");
     const clockObject = new DigitalClock(clockElement);
     clockObject.start();
-    
-}
-
-function startCount(){
-    currentStatus.isCounting = true;
-    currentStatus.isWorking = true;
-    let alarmHour = document.querySelector('#alarm-time-hour').value;
-    let alarmMinute = document.querySelector('#alarm-time-minute').value;
-    alarmTime.setHours(alarmHour);
-    alarmTime.setMinutes(alarmMinute);
-    alarmTime.setSeconds(0);
-    showingStatus();
-}
-
-function initialStatus(){
-    let worktime = Number(document.querySelector('#work-time').value);
-    let resttime = Number(document.querySelector('#rest-time').value);
-    currentStatus = new Status(worktime,resttime,false,0,false); 
-}
-
-function showingStatus(){
-    document.querySelector('#status').style.display = 'block';
-    document.querySelector('#alarmTime').textContent = addZero(alarmTime.getHours()) + ":" + addZero(alarmTime.getMinutes());
-    updateStopwatch();
-}
-
-function resetStatus(){
-    let worktime = Number(document.querySelector('#work-time').value);
-    let resttime = Number(document.querySelector('#rest-time').value);
-    currentStatus.workTime = worktime
-    currentStatus.restTime = resttime;
+    // ---------------------------------
+    initialBellTime();
 }
 class Status {
-    constructor(workTime, restTime,isWorking,totalTime,isCounting) {
-      this.workTime = workTime;
-      this.restTime = restTime;
+    constructor(isWorking,totalTime,isCounting) {
       this.isWorking = isWorking;
       this.totalTime = totalTime; 
       this.isCounting = isCounting;
     }
-  }
+}
+
 class DigitalClock {
     constructor(element) {
         this.element = element;
@@ -69,6 +41,68 @@ class DigitalClock {
         }
     }
 }
+function initialBellTime(){
+    document.querySelector('#alarm-time-hour').value = addZero(alarmTime.getHours());
+    document.querySelector('#alarm-time-minute').value = addZero(alarmTime.getMinutes());
+}
+
+function startBell(){
+    if(setupAlarmTime()){
+        document.querySelector('#isWorking').textContent = "準備中";
+        currentStatus.isCounting = true;
+        currentStatus.isWorking = true;
+        showingStatus();
+    }else{
+        alert("bell's time format isn't right");
+    }
+
+}
+
+function stopBell(){
+    currentStatus.isCounting = false;
+    currentStatus.isWorking = false;
+    document.querySelector('#isWorking').textContent = "停止中";
+    document.querySelector('#alarmTime').textContent = "00:00";
+}
+
+function setupAlarmTime(){
+    let alarmHour = document.querySelector('#alarm-time-hour').value;
+    let alarmMinute = document.querySelector('#alarm-time-minute').value;
+    let result = false;
+    if(validateHourFormat(alarmHour) && validateMinuteFormat(alarmMinute)){
+        alarmTime.setHours(addZero(alarmHour));
+        alarmTime.setMinutes(addZero(alarmMinute));
+        alarmTime.setSeconds(0);
+        result = true;
+    }
+    return result;
+}
+
+function validateHourFormat(hour) {
+    // Regular expression to match hour format: 00-23
+    const regex = /^(0\d|1\d|2[0-3])$/;
+    return regex.test(hour);
+  }
+  
+  function validateMinuteFormat(minute) {
+    // Regular expression to match minute format: 00-59
+    const regex = /^[0-5]\d$/;
+    return regex.test(minute);
+  }
+
+function showingStatus(){
+    document.querySelector('#status').style.display = 'block';
+    document.querySelector('#alarmTime').textContent = addZero(alarmTime.getHours()) + ":" + addZero(alarmTime.getMinutes());
+    updateStopwatch();
+}
+
+
+function updateStopwatch(){
+    const hours = addZero(Math.floor(currentStatus.totalTime / 3600));
+    const minutes = addZero(Math.floor((currentStatus.totalTime - hours * 3600) / 60));
+    document.querySelector('#totalTime').textContent = hours + ":" + minutes;
+}
+
 function checkTime(currentTime) {
     if (currentTime.getHours() == alarmTime.getHours() && currentTime.getMinutes() == alarmTime.getMinutes()) {
         alarm();
@@ -79,16 +113,16 @@ function alarm() {
     let audio;
     if (!currentStatus.isWorking) {
         currentStatus.isWorking = true;
-        console.log(currentStatus.isWorking);
         audio = new Audio('Abreak.mp3');
         playRing(audio);
-        resetAlarm(currentStatus.restTime);
+        let resttime = Number(document.querySelector('#rest-time').value);
+        resetAlarm(resttime);
     } else {
         currentStatus.isWorking = false;
-        console.log(currentStatus.isWorking);
         audio = new Audio('Beginning.mp3');
         playRing(audio);
-        resetAlarm(currentStatus.workTime);
+        let worktime = Number(document.querySelector('#work-time').value);
+        resetAlarm(worktime);
     }
 }
 
@@ -112,10 +146,4 @@ function resetAlarm(minute) {
     document.querySelector('#alarmTime').textContent = addZero(alarmTime.getHours()) + ":" + addZero(alarmTime.getMinutes());
     
     document.querySelector('#isWorking').textContent = (!currentStatus.isWorking) ? "工作中" : "休息" ;
-}
-
-function updateStopwatch(){
-    const hours = addZero(Math.floor(currentStatus.totalTime / 3600));
-    const minutes = addZero(Math.floor((currentStatus.totalTime - hours * 3600) / 60));
-    document.querySelector('#totalTime').textContent = hours + ":" + minutes;
 }
